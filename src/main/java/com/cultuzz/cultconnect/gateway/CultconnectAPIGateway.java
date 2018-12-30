c4df2de504c0683b2d30da1b801962669a66429a
Mahesh Kumar Routh <maheshkumar.routh@cultuzz.com>
2018-12-30 10:05:01 +0530

    first commit

--- src/main/java/com/cultuzz/cultconnect/gateway/CultconnectAPIGateway.java
+++ src/main/java/com/cultuzz/cultconnect/gateway/CultconnectAPIGateway.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
-@SpringBootApplication(scanBasePackages = "com.cultuzz")
+@SpringBootApplication
 public class CultconnectAPIGateway {
 
 	public static void main(String[] args) {
